3df24deca674bd1fe47c97bb609bbee64d9a2a85
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-05-01 15:41:03 +0100

    Implement AuthProvider and app metadata
    Added AuthProvider and metadata for the app.

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,18 @@
+import { AuthProvider } from "@/context/AuthContext";
+import "./globals.css";
+
+export const metadata = {
+  title: "Zwey",
+  description: "Social hub for upcoming artists",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
 
