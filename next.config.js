bf504f6efdf6a6e35ba7c2faad000b4a3d106ac3
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-05-06 23:51:27 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: false,
-  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false
+    };
+    return config;
+  }
 };
 
 module.exports = nextConfig;
